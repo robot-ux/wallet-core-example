05a1cf1c4d58a977c171bb2e1595f1e098e6442f
yolin <trimer.h@gmail.com>
2022-05-20 09:44:00 +0800

    chore: add test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { initWasm } from './initWasm';
+
+initWasm();
 
 function App() {
+  const handleCreateWallet = () => {
+    const WalletCore = (window as any).Module;
+    const wallet = new WalletCore.HDWallet.create(128, "");
+
+    console.log(wallet.mnemonic())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleCreateWallet}>Create Wallet</button>
     </div>
   );
 }

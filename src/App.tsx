db85887f2b138f2749fde8aa99f84b22f0f020fe
yolin <trimer.h@gmail.com>
2022-05-20 10:10:21 +0800

    feat: Show Mnemonic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { initWasm } from './initWasm';
-
-initWasm();
+import { useState } from 'react';
 
 function App() {
+  const [mnemonic, setMnemonic] = useState('')
+
   const handleCreateWallet = () => {
     const WalletCore = (window as any).Module;
     const wallet = new WalletCore.HDWallet.create(128, "");
 
-    console.log(wallet.mnemonic())
+    console.log(wallet.mnemonic());
+    setMnemonic(wallet.mnemonic());
   }
 
   return (
     <div>
       <button onClick={handleCreateWallet}>Create Wallet</button>
+      <div>Mnemonic: {mnemonic}</div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initWasm } from './initWasm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(<div>loading...</div>)
+
+initWasm().then(() => {
+  // rerender
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
